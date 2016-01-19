6b941edf19e23852b914922ba6b2817fb9e2cee0
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-01-19 08:40:39 +0100

    Changed jQuery to $

--- source/js/helpers/gallery.js
+++ source/js/helpers/gallery.js
@@ -1,6 +1,6 @@
 //
 // @name Gallery
-// @description  Popup boxes for gallery items. 
+// @description  Popup boxes for gallery items.
 //
 HelsingborgPrime = HelsingborgPrime || {};
 HelsingborgPrime.Helpers = HelsingborgPrime.Helpers || {};
@@ -8,58 +8,57 @@ HelsingborgPrime.Helpers = HelsingborgPrime.Helpers || {};
 HelsingborgPrime.Helpers.GalleryPopup = (function ($) {
 
     function ModalLimit() {
-    	
+
     	//Click event
     	this.clickWatcher();
-    	
-    	//Popup hash changes 
-    	jQuery(window).bind('hashchange', function() {
+
+    	//Popup hash changes
+    	$(window).bind('hashchange', function() {
 			this.togglePopupClass();
 		}.bind(this)).trigger('hashchange');
 
     }
 
     ModalLimit.prototype.clickWatcher = function () {
-	    
-	    jQuery('.lightbox-trigger').click(function(event) {
+
+	    $('.lightbox-trigger').click(function(event) {
 
 			event.preventDefault();
 
-			//Get data 
-			var image_href 		= jQuery(this).attr("href");
-			var image_caption 	= jQuery(this).attr("data-caption");
+			//Get data
+			var image_href 		= $(this).attr("href");
+			var image_caption 	= $(this).attr("data-caption");
 
 			//Update hash
-			window.location.hash = "lightbox-modal-open"; 
+			window.location.hash = "lightbox-modal-open";
 
-			//Add markup, or update. 
-			if (jQuery('#lightbox-modal').length > 0) {
-				jQuery('#lightbox-modal-image').attr('src',image_href);
-				jQuery('#lightbox-modal').show();
+			//Add markup, or update.
+			if ($('#lightbox-modal').length > 0) {
+				$('#lightbox-modal-image').attr('src',image_href);
+				$('#lightbox-modal').show();
 			} else {
-				
-				var lightbox = 
+
+				var lightbox =
 				'<div id="lightbox-modal">' +
 					'<div id="lightbox-image-wrapper" data-caption="' + image_caption + '">' +
 						'<a class="btn-close" href="#lightbox-modal-close"></a>' +
 						'<img id="lightbox-modal-image" src="' + image_href +'" />' +
 					'</div>' +
 				'</div>';
-					
-				jQuery('body').append(lightbox);
-				
+
+				$('body').append(lightbox);
+
 			}
-			
+
 		});
-	
-		jQuery(document).on('click', '#lightbox-modal', function() {
-			jQuery(this).hide();
+
+		$(document).on('click', '#lightbox-modal', function() {
+			$(this).hide();
 		});
-         
+
     };
 
     ModalLimit.prototype.togglePopupClass = function(){
-	    console.log("test");
 	    if (window.location.hash.indexOf('lightbox-modal-open') > 0 ) {
 			$('html').addClass('gallery-hidden');
 		} else {
