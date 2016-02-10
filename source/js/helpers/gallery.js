9f69d51d98a432ed504de766cf85a597d8cfd587
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-02-10 11:40:04 +0100

    Optimization adjustments

--- source/js/helpers/gallery.js
+++ source/js/helpers/gallery.js
@@ -1,6 +1,6 @@
 //
 // @name Gallery
-// @description  Popup boxes for gallery items. 
+// @description  Popup boxes for gallery items.
 //
 HelsingborgPrime = HelsingborgPrime || {};
 HelsingborgPrime.Helpers = HelsingborgPrime.Helpers || {};
@@ -8,11 +8,11 @@ HelsingborgPrime.Helpers = HelsingborgPrime.Helpers || {};
 HelsingborgPrime.Helpers.GalleryPopup = (function ($) {
 
     function ModalLimit() {
-    	
+
     	//Click event
     	this.clickWatcher();
-    	
-    	//Popup hash changes 
+
+    	//Popup hash changes
     	$(window).bind('hashchange', function() {
 			this.togglePopupClass();
 		}.bind(this)).trigger('hashchange');
@@ -20,47 +20,47 @@ HelsingborgPrime.Helpers.GalleryPopup = (function ($) {
     }
 
     ModalLimit.prototype.clickWatcher = function () {
-	    
+
 	    $('.lightbox-trigger').click(function(event) {
 
 			event.preventDefault();
 
-			//Get data 
+			//Get data
 			var image_href 		= $(this).attr("href");
 			var image_caption 	= $(this).attr("data-caption");
 
 			//Update hash
-			window.location.hash = "lightbox-open"; 
+			window.location.hash = "lightbox-open";
 
-			//Add markup, or update. 
+			//Add markup, or update.
 			if ($('#lightbox').length > 0) {
 				$('#lightbox-image').attr('src',image_href);
 				$('#lightbox').show(0);
 			} else {
-				
-				var lightbox = 
-				'<div id="lightbox">' +
-					'<div id="lightbox-image-wrapper" data-caption="' + image_caption + '">' +
+
+				var lightbox =
+				'<div class="lightbox">' +
+					'<div class="lightbox-image-wrapper" data-caption="' + image_caption + '">' +
 						'<a class="btn-close" href="#lightbox-close"></a>' +
 						'<img id="lightbox-image" src="' + image_href +'" />' +
 					'</div>' +
 				'</div>';
-					
+
 				$('body').append(lightbox);
-				
+
 			}
-			
-			//State 
-			HelsingborgPrime.Helpers.GalleryPopup.togglePopupClass(); 
-			
+
+			//State
+			HelsingborgPrime.Helpers.GalleryPopup.togglePopupClass();
+
 		});
-	
-		$(document).on('click', '#lightbox', function() {
+
+		$(document).on('click', '.lightbox', function() {
 			$(this).hide();
-			window.location.hash = "lightbox-closed"; 
-			HelsingborgPrime.Helpers.GalleryPopup.togglePopupClass(); 
+			window.location.hash = "lightbox-closed";
+			HelsingborgPrime.Helpers.GalleryPopup.togglePopupClass();
 		});
-         
+
     };
 
     ModalLimit.prototype.togglePopupClass = function(){
