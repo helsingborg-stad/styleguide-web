4b81c9b142973c5368bc77e40251f3299b22a010
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-04-05 09:16:37 +0200

    Arrow key navigation in gallery

--- source/js/component/gallery.js
+++ source/js/component/gallery.js
@@ -10,6 +10,7 @@ HelsingborgPrime.Component.GalleryPopup = (function ($) {
     function GalleryPopup() {
     	//Click event
     	this.clickWatcher();
+        this.arrowNav();
 
     	//Popup hash changes
     	$(window).bind('hashchange', function() {
@@ -18,23 +19,19 @@ HelsingborgPrime.Component.GalleryPopup = (function ($) {
 
         //Preload on hover
         this.preloadImageAsset();
-
     }
 
     GalleryPopup.prototype.clickWatcher = function () {
-
 	    $('.lightbox-trigger').click(function(event) {
-
 			event.preventDefault();
 
 			//Get data
-			var image_href 		= $(this).attr("href");
+			var image_href = $(this).attr('href');
+            var image_caption = '';
 
             //Get caption
-            if( typeof $(this).attr("data-caption") === 'undefined' ) {
-                var image_caption = "";
-            } else {
-               var image_caption = $(this).attr("data-caption");
+            if (typeof $(this).attr('data-caption') !== 'undefined') {
+                var image_caption = $(this).attr("data-caption");
             }
 
 			//Update hash
@@ -42,11 +39,10 @@ HelsingborgPrime.Component.GalleryPopup = (function ($) {
 
 			//Add markup, or update.
 			if ($('#lightbox').length > 0) {
-				$('#lightbox-image').attr('src',image_href);
+                $('#lightbox-image').attr('src',image_href);
                 $('#lightbox .lightbox-image-wrapper').attr('data-caption',image_caption);
-				$('#lightbox').fadeIn();
+                $('#lightbox').fadeIn();
 			} else {
-
 				var lightbox =
 				'<div id="lightbox">' +
 					'<div class="lightbox-image-wrapper" data-caption="' + image_caption + '">' +
@@ -59,28 +55,72 @@ HelsingborgPrime.Component.GalleryPopup = (function ($) {
                 $('#lightbox').hide().fadeIn();
 			}
 
+            $(this).addClass('gallery-active');
 		});
 
-		$(document).on('click', '#lightbox', function() {
+		$(document).on('click', '#lightbox', function () {
 			$(this).fadeOut(300).hide(0);
-			window.location.hash = "lightbox-closed";
+            $('.gallery-active').removeClass('gallery-active');
+			window.location.hash = 'lightbox-closed';
 		});
 
     };
 
-    GalleryPopup.prototype.togglePopupClass = function(){
-	    if (window.location.hash.replace("-","") == "#lightbox-open".replace("-","")) {
+    GalleryPopup.prototype.togglePopupClass = function (){
+	    if (window.location.hash.replace('-', '') == '#lightbox-open'.replace('-', '')) {
 			$('html').addClass('gallery-hidden');
 		} else {
 			$('html').removeClass('gallery-hidden');
 		}
     };
 
-    GalleryPopup.prototype.preloadImageAsset = function(){
-        $(".image-gallery a.lightbox-trigger").on("mouseenter", function(){
+    GalleryPopup.prototype.preloadImageAsset = function () {
+        $('.image-gallery a.lightbox-trigger').on('mouseenter', function(){
             var img = new Image();
-            img.src = jQuery(this).attr("href");
+            img.src = jQuery(this).attr('href');
         });
+    };
+
+    GalleryPopup.prototype.arrowNav = function () {
+        // Keycodes
+        var leftArrow = 37;
+        var rightArrow = 39;
+
+        $(window).on('keyup', function (e) {
+            if (window.location.hash.replace('-', '') != '#lightbox-open'.replace('-', '')) {
+                return false;
+            }
+
+            if (e.which == leftArrow) {
+                this.prevImg();
+            } else if (e.which == rightArrow) {
+                this.nextImg();
+            }
+        }.bind(this));
+    };
+
+    GalleryPopup.prototype.nextImg = function () {
+        var nextImg = $('.gallery-active').parent('li').next().children('a');
+        if (nextImg.length == 0) {
+            nextImg = $('.gallery-active').parents('ul').children('li:first-child').children('a');
+        }
+
+        $('#lightbox').trigger('click');
+        setTimeout(function () {
+            nextImg.trigger('click');
+        }, 100);
+    };
+
+    GalleryPopup.prototype.prevImg = function () {
+        var prevImg = $('.gallery-active').parent('li').prev().children('a');
+        if (prevImg.length == 0) {
+            prevImg = $('.gallery-active').parents('ul').children('li:last-child').children('a');
+        }
+
+        $('#lightbox').trigger('click');
+        setTimeout(function () {
+            prevImg.trigger('click');
+        }, 100);
     };
 
     new GalleryPopup();
