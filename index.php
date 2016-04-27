4ddeef6941348af22f1c46f170edea7a4b33387f
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-04-27 14:34:57 +0200

    Coestyle fix

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 
-//Enable/disable all errors 
-if(true){
-	error_reporting(E_ALL);
-	ini_set('error_reporting', E_ALL);
+//Enable/disable all errors
+if (true) {
+    error_reporting(E_ALL);
+    ini_set('error_reporting', E_ALL);
 }
 
-//Run application 
+//Run application
 require_once 'Bootstrap.php';
 new \HbgStyleGuide\App();
