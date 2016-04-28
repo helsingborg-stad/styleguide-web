66ed227fef51224da5b162a18b9fd327e3620b95
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-04-28 10:49:28 +0200

    No need for gulp-plumber

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,6 @@ var uglify = require('gulp-uglify');
 var cssnano = require('gulp-cssnano');
 var rename = require('gulp-rename');
 var autoprefixer = require('gulp-autoprefixer');
-var plumber = require('gulp-plumber');
 var dss = require('gulp-docs');
 
 var node_modules = 'node_modules/';
@@ -16,7 +15,6 @@ var node_modules = 'node_modules/';
 // Compile Our Sass
 gulp.task('sass-dist', function() {
     return gulp.src('source/sass/hbg-prime.scss')
-            .pipe(plumber())
             .pipe(sass())
             .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
             .pipe(rename({suffix: '.min'}))
@@ -29,7 +27,6 @@ gulp.task('sass-dist', function() {
 
 gulp.task('sass-dev', function() {
     return gulp.src('source/sass/hbg-prime.scss')
-            .pipe(plumber())
             .pipe(sass({ sourceComments: true }))
             .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
             .pipe(rename({suffix: '.dev'}))
