8af585f56d983665c6a9f122366b97e73278ce61
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-04-29 10:06:01 +0200

    Menu priority script fix

--- source/js/helper/menuPriority.js
+++ source/js/helper/menuPriority.js
@@ -13,6 +13,7 @@ HelsingborgPrime.Helper.MenuPriority = (function ($) {
 
     var availableSpace = 0;
     var breaks = [];
+    var breakWasTwoOrMore = false;
 
     function MenuPriority() {
         if ($('.header-jumbo').length > 0 && !$('#main-menu').hasClass('nav-justify')) {
@@ -35,6 +36,11 @@ HelsingborgPrime.Helper.MenuPriority = (function ($) {
 
     MenuPriority.prototype.updateNavigation = function () {
         availableSpace = $btn.is(':visible') ? $nav.parent().first().width() - ($btn.width() + parseFloat($nav.attr('data-btn-width'))) : $nav.parent().first().width();
+
+        if (breaks.length == 1 && breakWasTwoOrMore === true) {
+            availableSpace = $nav.parent().first().width();
+            breakWasTwoOrMore= false;
+        }
 
         // The visible list is overflowing the available space
         if ($vlinks.width() > 0 && $vlinks.width() > availableSpace) {
@@ -61,13 +67,9 @@ HelsingborgPrime.Helper.MenuPriority = (function ($) {
             }
         }
 
-        /*
-        console.table({
-            'visible-links' : $vlinks.width(),
-            'available-space' : availableSpace,
-            'breaks' : breaks.length
-        });
-        */
+        if (breaks.length > 1) {
+            breakWasTwoOrMore = true;
+        }
 
         // Rerun if nav is still overflowing
         if ($nav.is(':visible') && $vlinks.width() > availableSpace && breaks.length > 0 && breaks.length < $vlinks.children('li').length) {
