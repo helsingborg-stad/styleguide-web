bba370ef67a06484bd619582b70d39ae82a08c3d
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-05-19 16:19:07 +0200

    Improved image upload logic

--- source/js/component/image-upload.js
+++ source/js/component/image-upload.js
@@ -7,20 +7,35 @@ HelsingborgPrime.Component = HelsingborgPrime.Component || {};
 
 HelsingborgPrime.Component.ImageUpload = (function ($) {
 
+    var elementClass = '.image-upload';
     var drags = 0;
-    var droppedFiles = new Array();
-    var allowedFileTypes = ['image/svg+xml'];
+    var selectedFiles = new Array();
+    var allowedFileTypes = ['image/jpeg', 'image/png', 'image/gif'];
 
     function ImageUpload() {
-        this.init();
+        this.initDragAndDrop();
+        this.initFileInput();
     }
 
-    ImageUpload.prototype.handleEvents = function () {
+    /**
+     * Select file by browse
+     * @return {void}
+     */
+    ImageUpload.prototype.initFileInput = function () {
+        $imageUploadInput = $(elementClass).find('input[type="file"]');
 
+        $imageUploadInput.on('change', function (e) {
+            var file = $(e.target).closest('input[type="file"]').get(0).files[0];
+            this.addFile($(e.target).closest(elementClass), file);
+        }.bind(this));
     };
 
-    ImageUpload.prototype.init = function () {
-        $imageUpload = $('.image-upload');
+    /**
+     * Drag and drop a file
+     * @return {void}
+     */
+    ImageUpload.prototype.initDragAndDrop = function () {
+        $imageUpload = $(elementClass);
 
         $imageUpload.on('drag dragstart dragend dragover dragenter dragleave drop', function (e) {
             e.preventDefault();
@@ -48,21 +63,55 @@ HelsingborgPrime.Component.ImageUpload = (function ($) {
                 $imageUpload.removeClass('is-selected is-dragover');
             }
 
-            droppedFiles = e.originalEvent.dataTransfer.files;
+            this.addFile($(e.target).closest(elementClass), e.originalEvent.dataTransfer.files[0]);
+        }.bind(this));
+    };
 
-            if (allowedFileTypes.indexOf(droppedFiles[droppedFiles.length-1].type) == -1) {
-                droppedFiles = false;
-                $imageUpload.addClass('is-error is-error-filetype');
-                $imageUpload.find('.selected-file').html('');
-                $imageUpload.find('input[type="file"]').prop('files', '');
-                return false;
+    /**
+     * Adds a file
+     * @param {object} element The image uploader element
+     * @param {object} file    The file object
+     */
+    ImageUpload.prototype.addFile = function (element, file) {
+        if (allowedFileTypes.indexOf(file.type) == -1) {
+            element.addClass('is-error is-error-filetype');
+            element.find('.selected-file').html('');
+
+            return false;
+        }
+
+        var maxFilesize = element.attr('data-max-size') ? element.attr('data-max-size') : 500;
+        var fileSize = (file.size/1000).toFixed(2);
+
+        if (fileSize > maxFilesize) {
+            element.addClass('is-error is-error-filesize');
+            element.find('.selected-file').html('');
+
+            return false;
+        }
+
+        selectedFiles.push(file);
+        element.find('.selected-file').html(file.name);
+        element.addClass('is-selected');
+
+        var reader = new FileReader();
+        reader.readAsDataURL(file);
+
+        reader.addEventListener('load', function (e) {
+            var image = e.target;
+            var max_images = element.attr('data-max-files');
+
+
+            if (max_images && selectedFiles.length > max_images) {
+                selectedFiles = selectedFiles.slice(1);
+                element.find('input[name="image_uploader_file[]"]:first').remove();
             }
 
-            $imageUpload.find('input[type="file"]').prop('files', droppedFiles);
-
-            $imageUpload.find('.selected-file').html(droppedFiles[droppedFiles.length-1].name);
-            $imageUpload.addClass('is-selected');
+            element.append('<input type="hidden" name="image_uploader_file[]" read-only>');
+            element.find('input[name="image_uploader_file[]"]:last').val(image.result);
         });
+
+        return true;
     };
 
     return new ImageUpload();
