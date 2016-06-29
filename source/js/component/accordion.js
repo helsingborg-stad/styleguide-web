a57f9c50850acf9f1ec50c82d6423675656cd4af
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-06-29 14:05:43 +0200

    Accordion fix

--- source/js/component/accordion.js
+++ source/js/component/accordion.js
@@ -12,9 +12,8 @@ HelsingborgPrime.Component.Accordion = (function ($) {
     }
 
     Accordion.prototype.init = function () {
-        $('div.accordion label').on('click', function(event) {
-
-				window.location.hash = '#' + $(this).attr('for');
+        $('.accordion-toggle').on('click', function(event) {
+			window.location.hash = '#' + $(this).attr('for');
 		});
 
 
