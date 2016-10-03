e531e583460c313fd157f94052f398b4c94b94d2
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-10-03 11:04:48 +0200

    Dropdown close icon

--- source/js/component/dropdown.js
+++ source/js/component/dropdown.js
@@ -16,9 +16,22 @@ HelsingborgPrime.Component.Dropdown = (function ($) {
             e.preventDefault();
 
             var targetElement = $(this).attr('data-dropdown');
+            $(targetElement).toggleClass('dropdown-target-open');
             $(this).toggleClass('dropdown-open');
             $(this).parent().find(targetElement).toggle();
             $(this).parent().find(targetElement).find('input[data-dropdown-focus]').focus();
+        });
+
+        $('body').on('click', function (e) {
+            var $target = $(e.target);
+
+            if ($target.closest('.dropdown-target-open').length || $target.closest('[data-dropdown]').length) {
+                return;
+            }
+
+            $('[data-dropdown].dropdown-open').removeClass('dropdown-open');
+            $('.dropdown-target-open').toggle();
+            $('.dropdown-target-open').removeClass('dropdown-target-open');
         });
     };
 
