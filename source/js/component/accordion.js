5fe93a652659b6d3d611f90f654cac9d7d295c81
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-10-06 08:26:51 +0200

    Method to close radio based accordion

--- source/js/component/accordion.js
+++ source/js/component/accordion.js
@@ -13,13 +13,23 @@ HelsingborgPrime.Component.Accordion = (function ($) {
 
     Accordion.prototype.init = function () {
         $('label.accordion-toggle').on('click', function(e) {
-            var input = $('#' + $(this).attr('for'));
+            var $input = $('#' + $(this).attr('for'));
 
-            if (input.prop('checked') === false) {
+            if ($input.prop('checked') === false) {
                 window.location.hash = '#' + $(this).attr('for');
             } else {
-                if (!input.is('[type="checkbox"]')) {
-                    return;
+                if ($input.is('[type="radio"]')) {
+                    var name = $input.attr('name');
+                    var value = $input.val();
+                    var id = $input.attr('id');
+
+                    var $parent = $input.parent('section');
+                    $input.remove();
+
+                    setTimeout(function () {
+                        $parent.prepend('<input type="radio" name="' + name + '" value="' + value + '" id="' + id + '">');
+                    }, 1);
+
                 }
 
                 window.location.hash = '_';
