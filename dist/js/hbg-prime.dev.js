a62b660efa8bf67ec04a9e123a385f6f81e030e5
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2017-01-09 14:20:30 +0100

    Add sticky scroll js

--- dist/js/hbg-prime.dev.js
+++ dist/js/hbg-prime.dev.js
@@ -16251,6 +16251,110 @@ HelsingborgPrime.Helper.ScrollElevator = (function ($) {
 
 })(jQuery);
 
+//
+// @name Local link
+// @description  Finds link items with outbound links and gives them outbound class
+//
+HelsingborgPrime = HelsingborgPrime || {};
+HelsingborgPrime.Helper = HelsingborgPrime.Helper || {};
+
+HelsingborgPrime.Helper.StickyScroll = (function ($) {
+
+    var _stickyElements = [];
+    var _isFloatingClass = 'is-sticky-scroll';
+
+    function StickyScroll() {
+        $('.sticky-scroll').each(function (index, element) {
+            this.init(element);
+        }.bind(this));
+
+        $(document).on('scroll', function () {
+            this.scrolling();
+        }.bind(this));
+    }
+
+    /**
+     * Initializes the sticky-scroll functionallity on element
+     * @param  {object} element
+     * @return {void}
+     */
+    StickyScroll.prototype.init = function(element) {
+        var $element = $(element).closest('.sticky-scroll');
+        var offsetTop = $(element).offset().top;
+
+        _stickyElements.push({
+            element: $element,
+            originalOffset: null
+        });
+    }
+
+    /**
+     * Handles actions during scroll
+     * @return {void}
+     */
+    StickyScroll.prototype.scrolling = function() {
+        var scrollPos = $(document).scrollTop();
+
+        _stickyElements.forEach(function (item) {
+            var $element = $(item.element);
+
+            if (
+                (item.originalOffset === null && scrollPos >= $element.offset().top)
+                ||
+                (item.originalOffset !== null && scrollPos >= item.originalOffset)
+            ) {
+                if (!$element.hasClass(_isFloatingClass)) {
+                    item.originalOffset = $element.offset().top;
+                    this.addPlaceholder(item.element);
+                    item.element.addClass(_isFloatingClass);
+                }
+
+                return
+            }
+
+            // Resets the floating class if it shouldnt float
+            if (item.element.hasClass(_isFloatingClass)) {
+                item.originalOffset = null;
+                item.element.removeClass(_isFloatingClass);
+                this.removePlaceholder(item.element);
+            }
+        }.bind(this));
+    };
+
+    /**
+     * Adds a placeholder to keep the dom from "jumping"
+     * @param {object} element
+     */
+    StickyScroll.prototype.addPlaceholder = function(element) {
+        var $ghost = element.clone();
+        $ghost.addClass(_isFloatingClass + '-placeholder');
+
+        // Hide the ghost placeholder
+        $ghost.css({
+            visibility: 'hidden'
+        });
+
+        // Make sure to disable all type of form elements in placeholder
+        $ghost.find('form, input, select, textarea').prop('disabled', true);
+
+        // Insert the placeholder to the dom
+        $ghost.insertBefore(element);
+    };
+
+    /**
+     * Removes the placeholder
+     * @param  {object} element Element
+     * @return {void}
+     */
+    StickyScroll.prototype.removePlaceholder = function(element) {
+        var $ghost = element.prev('.' + _isFloatingClass + '-placeholder');
+        $ghost.remove();
+    };
+
+    return new StickyScroll();
+
+})(jQuery);
+
 HelsingborgPrime = HelsingborgPrime || {};
 HelsingborgPrime.Helper = HelsingborgPrime.Helper || {};
 
@@ -16466,8 +16570,9 @@ HelsingborgPrime.Prompt = HelsingborgPrime.Prompt || {};
 
 HelsingborgPrime.Prompt.CookieConsent = (function ($) {
 
-    var useLocalStorage = true;
-    var animationSpeed = 1000;
+    var _cookieConsentVisible = false;
+    var _useLocalStorage = true;
+    var _animationSpeed = 1000;
 
     function CookieConsent() {
         this.init();
@@ -16517,10 +16622,11 @@ HelsingborgPrime.Prompt.CookieConsent = (function ($) {
         ');
 
         $('#cookie-consent').show();
+        _cookieConsentVisible = true;
     };
 
     CookieConsent.prototype.hasAccepted = function() {
-        if (useLocalStorage) {
+        if (_useLocalStorage) {
             return window.localStorage.getItem('cookie-consent');
         } else {
             return HelsingborgPrime.Helper.Cookie.check('cookie-consent', true);
@@ -16529,8 +16635,9 @@ HelsingborgPrime.Prompt.CookieConsent = (function ($) {
 
     CookieConsent.prototype.accept = function() {
         $('#cookie-consent').remove();
+        _cookieConsentVisible = false;
 
-        if (useLocalStorage) {
+        if (_useLocalStorage) {
             try {
                 window.localStorage.setItem('cookie-consent', true);
                 return true;
