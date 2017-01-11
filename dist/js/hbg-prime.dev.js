f9a1cd69e6ea0e2787b2764abf7f048a2fbf4c42
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2017-01-11 10:51:15 +0100

    Rename list filter to table filter

--- dist/js/hbg-prime.dev.js
+++ dist/js/hbg-prime.dev.js
@@ -15986,38 +15986,6 @@ HelsingborgPrime.Helper.Input = (function ($) {
 
 })(jQuery);
 
-HelsingborgPrime = HelsingborgPrime || {};
-HelsingborgPrime.Helper = HelsingborgPrime.Helper || {};
-
-HelsingborgPrime.Helper.TableFilter = (function ($) {
-
-    function TableFilter() {
-        $('[data-table-filter]').each(function (index, element) {
-            this.init(element);
-        }.bind(this));
-    }
-
-    TableFilter.prototype.init = function(element) {
-        var $list = $(element);
-        var listId = $list.attr('data-table-filter');
-        var $input = $('[data-table-filter-input="' + listId + '"]');
-
-        $input.on('input', function (e) {
-            $list.find('[data-table-filter-empty]').remove();
-
-            $list.find('tbody tr:not([data-table-filter-exclude]):contains(' + $input.val() + ')').show();
-            $list.find('tbody tr:not([data-table-filter-exclude]):not(:contains(' + $input.val() + '))').hide();
-
-            if ($list.find('tbody tr:not([data-table-filter-exclude]):visible').length === 0 && $list.find('[data-table-filter-empty]').length === 0) {
-                $list.find('tbody tr:not([data-table-filter-exclude]):first').before('<tr data-table-filter-empty><td colspan="50">' + HelsingborgPrime.Args.get('tableFilter.empty') + '</td></tr>')
-            }
-        });
-    };
-
-    return new TableFilter();
-
-})(jQuery);
-
 //
 // @name Local link
 // @description  Finds link items with outbound links and gives them outbound class
@@ -16403,6 +16371,38 @@ HelsingborgPrime.Helper.StickyScroll = (function ($) {
     };
 
     return new StickyScroll();
+
+})(jQuery);
+
+HelsingborgPrime = HelsingborgPrime || {};
+HelsingborgPrime.Helper = HelsingborgPrime.Helper || {};
+
+HelsingborgPrime.Helper.TableFilter = (function ($) {
+
+    function TableFilter() {
+        $('[data-table-filter]').each(function (index, element) {
+            this.init(element);
+        }.bind(this));
+    }
+
+    TableFilter.prototype.init = function(element) {
+        var $list = $(element);
+        var listId = $list.attr('data-table-filter');
+        var $input = $('[data-table-filter-input="' + listId + '"]');
+
+        $input.on('input', function (e) {
+            $list.find('[data-table-filter-empty]').remove();
+
+            $list.find('tbody tr:not([data-table-filter-exclude]):contains(' + $input.val() + ')').show();
+            $list.find('tbody tr:not([data-table-filter-exclude]):not(:contains(' + $input.val() + '))').hide();
+
+            if ($list.find('tbody tr:not([data-table-filter-exclude]):visible').length === 0 && $list.find('[data-table-filter-empty]').length === 0) {
+                $list.find('tbody tr:not([data-table-filter-exclude]):first').before('<tr data-table-filter-empty><td colspan="50">' + HelsingborgPrime.Args.get('tableFilter.empty') + '</td></tr>')
+            }
+        });
+    };
+
+    return new TableFilter();
 
 })(jQuery);
 
