697256534e9fc844960ef20fa3cae5881c2fe364
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2017-01-31 10:30:05 +0100

    Makes stickyscroll admin-bar aware

--- source/js/helper/StickyScroll.js
+++ source/js/helper/StickyScroll.js
@@ -42,6 +42,14 @@ HelsingborgPrime.Helper.StickyScroll = (function ($) {
     StickyScroll.prototype.scrolling = function() {
         var scrollOffset = $(window).scrollTop();
 
+        if ($('body').hasClass('admin-bar')) {
+            scrollOffset += 32;
+
+            if ($(window).width() < 783) {
+                scrollOffset += 14;
+            }
+        }
+
         $.each(_stickyElements, function (index, item) {
             if (scrollOffset > item.offsetTop) {
                 return this.stick(item.element);
