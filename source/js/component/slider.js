c0cdea6835b6a06ded0bd6a3a4b0493da5df2745
Kristoffer Svanmark <kristoffer@svanmark.se>
2017-02-08 08:59:26 +0100

    Slider is-collapsed and preload

--- source/js/component/slider.js
+++ source/js/component/slider.js
@@ -10,19 +10,66 @@ HelsingborgPrime.Component.Slider = (function ($) {
     var autoslideIntervals = [];
 
     function Slider() {
-        $('.slider').each(function () {
-            if ($(this).find('[data-flickity]')) {
+        this.preloadImage();
+
+        $('.slider').each(function (index, element) {
+            var $slider = $(element);
+
+            this.detectIfIsCollapsed(element);
+
+            if ($slider.find('[data-flickity]')) {
                 return;
             }
 
-            $(this).flickity({
+            $slider.flickity({
                 cellSelector: '.slide',
                 cellAlign: 'center',
                 setGallerySize: false,
                 wrapAround: true,
             });
-        });
+        }.bind(this));
+
+        $(window).resize(function() {
+            $('.slider').each(function (index, element) {
+                this.detectIfIsCollapsed(element);
+            }.bind(this));
+        }.bind(this));
     }
+
+    /**
+     * Add collapsed class
+     */
+    Slider.prototype.detectIfIsCollapsed = function (slider) {
+        if ($(slider).width() <= 500) {
+            $(slider).addClass("is-collapsed");
+            return;
+        }
+
+        $(slider).removeClass("is-collapsed");
+    };
+
+    Slider.prototype.preloadImage = function () {
+        setTimeout(function(){
+
+            var normal_img = [];
+            var mobile_img = [];
+
+            $(".slider .slide").each(function(index, slide) {
+
+                if ($(".slider-image-mobile", slide).length) {
+                    normal_img.index = new Image();
+                    normal_img.index.src = $(".slider-image-desktop", slide).css('background-image').replace(/.*\s?url\([\'\"]?/, '').replace(/[\'\"]?\).*/, '');
+                }
+
+                if ($(".slider-image-mobile", slide).length) {
+                    mobile_img.index = new Image();
+                    mobile_img.index.src = $(".slider-image-mobile", slide).css('background-image').replace(/.*\s?url\([\'\"]?/, '').replace(/[\'\"]?\).*/, '');
+                }
+
+            });
+
+        },5000);
+    };
 
     return new Slider();
 
