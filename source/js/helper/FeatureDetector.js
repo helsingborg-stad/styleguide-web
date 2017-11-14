9af8cb24c9990bce6705b29fd7b36901f7bbfde3
Sebastian Thulin <sebastian.thulin@gmail.com>
2017-11-14 11:08:13 +0100

    Detects webp support

--- source/js/helper/FeatureDetector.js
+++ source/js/helper/FeatureDetector.js
@@ -4,7 +4,9 @@ HelsingborgPrime.Helper = HelsingborgPrime.Helper || {};
 HelsingborgPrime.Helper.FeatureDetector = (function ($) {
 
     function FeatureDetector() {
+        console.log("testing");
         this.detectFlexbox();
+        this.detectWebp();
     }
 
     FeatureDetector.prototype.detectFlexbox = function () {
@@ -14,6 +16,23 @@ HelsingborgPrime.Helper.FeatureDetector = (function ($) {
 
         $('html').addClass('no-flexbox');
         return false;
+    };
+
+    FeatureDetector.prototype.detectWebp = function () {
+
+        var webPsupport = (function() {
+          var webP = new Image();
+          webP.onload = WebP.onerror = function () {
+            callback(webP.height == 2);
+          };
+          webP.src = 'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';
+        });
+
+        if(webPsupport) {
+            $('html').addClass('webp');
+        } else {
+            $('html').addClass('no-webp');
+        }
     };
 
     return new FeatureDetector();
