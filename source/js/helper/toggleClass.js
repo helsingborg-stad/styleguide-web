544fbdb4cb0267c73aafe9c7cdab5c66f6894b9a
Sebastian Thulin <sebastian.thulin@gmail.com>
2018-04-12 12:45:37 +0200

    Adds toggle class

---
+++ source/js/helper/toggleClass.js
@@ -0,0 +1,16 @@
+//
+// @name Local link
+// @description  Finds link items with outbound links and gives them outbound class
+//
+HelsingborgPrime = HelsingborgPrime || {};
+HelsingborgPrime.Helper = HelsingborgPrime.Helper || {};
+
+HelsingborgPrime.Helper.toggleClass = (function ($) {
+
+    //Init
+    function toggleClass() {
+    }
+
+    return new toggleClass();
+
+})();
