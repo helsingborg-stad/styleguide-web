978a3d72a3f2f826c0b45fc724f5a5a48a7908e0
Carl Johan Silvergrund <johan.silvergrund@hiq.se>
2018-05-30 15:17:09 +0200

    gulp

--- source/js/app.js
+++ source/js/app.js
@@ -78,7 +78,6 @@ jQuery.expr[':'].icontains = function (a, i, m) {
     return jQuery(a).text().toUpperCase()
         .indexOf(m[3].toUpperCase()) >= 0;
 };
-
 /**
  *  Modularity - Post filters  - Toogle
  * */
