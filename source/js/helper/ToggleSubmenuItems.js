234148712fbc43992297ac20666ddd7a8543e887
Carl Johan Silvergrund <johan.silvergrund@hiq.se>
2018-10-10 16:46:47 +0200

    merge

--- source/js/helper/ToggleSubmenuItems.js
+++ source/js/helper/ToggleSubmenuItems.js
@@ -44,7 +44,7 @@ HelsingborgPrime.Helper.ToggleSubmenuItems = (function ($) {
             url: HbgPrimeArgs.api.root + 'municipio/v1/navigation/' + parentId,
             method: 'GET',
             cache: false,
-            dataType: 'html', 
+            dataType: 'html',
             beforeSend: function (xhr) {
                 xhr.setRequestHeader('X-WP-Nonce', HbgPrimeArgs.api.nonce);
             }
