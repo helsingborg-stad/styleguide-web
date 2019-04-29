4045ea00ba2691edf1486f4bb3092238f12f4a70
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2019-04-29 09:17:12 +0200

    Updated install routine

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ -d "source/library" ]; then
+if [ -d "v2" ]; then
     echo Version 2.0 repository already installed in /v2/. You might want to add this in your git-client-ui.
 else
-    git clone "git@github.com:helsingborg-stad/styleguide.git" "v2"
+    git clone "https://github.com/helsingborg-stad/styleguide.git" "v2"
 fi
